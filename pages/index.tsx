308c2bd5d50e6b7894bd7334e960beb348160409
Jeremy Smith <st.jeremy1@gmail.com>
2022-12-01 09:01:49 +0100

    Fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,8 @@
 import Head from 'next/head';
-
+import { Box } from '@chakra-ui/react';
+import Image from 'next/image';
 
 export default function Home() {
-
-  // const [ session, loading ] = useSession();
 
   return (
     <div>
@@ -13,9 +12,17 @@ export default function Home() {
         <link rel="icon" href="/logo.png" />
       </Head>
 
-      <main>
-        <h1>Hi everyone</h1>
+      <main >
+        <Box margin={'2%'}>
+          <Image src="/logo.png" width={ 50 } height={ 50 } alt='Logo' />
+        </Box>
+
+        <Box bg={'blue'} height={ 600 } margin={ '10%' } padding={ '5%'}>
+          <label htmlFor="Github">GitHub Sign In</label>
+          <button>Sign In</button>
+        </Box>
       </main>
+
     </div>
   )
 }
